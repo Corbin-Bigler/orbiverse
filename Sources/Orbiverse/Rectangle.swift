bbdc7ad021621c13b0b2c6cf5523d8b05f1ed2ed
Corbin Bigler <77686907+Corbin-Bigler@users.noreply.github.com>
2024-07-30 21:44:36 -0600

    Update Rectangle.swift

--- Sources/Orbiverse/Rectangle.swift
+++ Sources/Orbiverse/Rectangle.swift
@@ -4,6 +4,11 @@ public struct Rectangle: Shape {
     public var position: Point
     public let size: Vector
     
+    public init(position: Point, size: Vector) {
+        self.position = position
+        self.size = size
+    }
+    
     public var left: Float { position.x }
     public var right: Float { position.x + size.width }
     public var top: Float { position.y }
